b178395ae447929386a2c96c8d530950fe61c824
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-05-02 03:22:09 -0300

    Update 0034.py

--- 0034.py
+++ 0034.py
@@ -1,23 +1,23 @@
 class Solution(object):
     def searchRange(self, nums, target):
-        res = []
-        length_res = len(res)
         counter = nums.count(target)
         if counter == 0:
-            result = [-1, -1]
-            return result
+            return [-1, -1]
         elif counter == 1:
             index = nums.index(target)
-            result = [target, target]
-            return result
+            return [index, index] # or [index] * 2
         else:
-            min_index = 10000000
-            max_index = -10000000
+            res = []
+            revNums = nums[::-1]
+            min_index = -1
+            max_index = 0
             for i, num in enumerate(nums):
-                if num == target and num < min_index:
+                if num == target:
                     min_index = i
-                if num == target and num > max_index:
-                    max_index = i
+                    break
+            for j, num in enumerate(revNums):
+                if nums[j] == target:
+                    max_index = j
             res.append(min_index)
             res.append(max_index)
             return res
