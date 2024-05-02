65faf891153094103587d75eeb5471bf162f32c9
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-05-02 03:40:26 -0300

    Update 0034.py

--- 0034.py
+++ 0034.py
@@ -21,3 +21,18 @@ class Solution(object):
             res.append(min_index)
             res.append(max_index)
             return res
+"""
+# a short and compact version (runtime is bigger than the previous version)
+class Solution(object):
+    def searchRange(self, nums, target):
+        list_of_index = []
+        for i, num in enumerate(nums):
+            if nums[i] == target:
+                list_of_index.append(i)
+        if len(list_of_index) == 0:
+            return [-1, -1]
+        elif len(list_of_index) == 1:
+            return list_of_index*2
+        else:
+            return [min(list_of_index), max(list_of_index)]
+"""
