07a8598f3bb12968a65d33a669cd13e10e32ae60
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-05-04 10:08:19 -0300

    Update 1351.py

--- 1351.py
+++ 1351.py
@@ -16,3 +16,16 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+"""
+# version2.0
+
+class Solution(object):
+    def countNegatives(self, grid):
+        counter = 0
+        for i in grid:
+            for j in i:
+                if j < 0:
+                    counter += 1
+        return counter
+"""
