f791d08bfa4ac19018a72cfecacc6ee575597865
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-05-17 02:06:15 -0300

    Add files via upload

---
+++ 2001.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def interchangeableRectangles(self, rectangles):
+        counter = 0
+        n = len(rectangles)
+        for i in range(len(rectangles)-1):
+            for j in range(i + 1, n):
+                if (rectangles[i][0] / rectangles[i][1]) == (rectangles[j][0] / rectangles[j][1]):
+                    counter += 1
+        return counter
+
+
+def main() -> None:
+    sol = Solution()
+    rectangles = [[1,7],[2,8],[8,8],[2,5],[2,8],[7,4]]
+    print(sol.interchangeableRectangles(rectangles))
+
+
+if __name__ == "__main__":
+    main()
+
