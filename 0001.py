cc9df1ba04d13e98613719e654612804f1c889b7
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-05-19 09:31:34 -0300

    New version added

--- 0001.py
+++ 0001.py
@@ -8,3 +8,15 @@ class Solution(object):
                 if i != j and nums[i] + nums[j] == target:
                     return i, j
         return 0    
+
+
+# V2:
+class Solution(object):
+    def twoSum(self, nums, target):
+        res = []
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    res.append(i)
+                    res.append(j)
+        return res
