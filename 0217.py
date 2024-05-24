d07c81afe0bb1e696473727041f183e2e50c606c
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-05-24 16:53:23 -0300

    Update 0217.py

--- 0217.py
+++ 0217.py
@@ -5,3 +5,27 @@ class Solution(object):
             if nums[i] == nums[i+1]:
                 return True
         return False
+
+# V2
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashtable = {}
+        for num in nums:
+            if num in hashtable:
+                hashtable[num] += 1
+            else:
+                hashtable[num] = 1
+        if max(hashtable.values()) > 1:
+            return True
+        else:
+            return False
+
+def main() -> None:
+    sol = Solution()
+    nums = [1,2,3,4]
+    print(sol.containsDuplicate(nums))
+
+
+if __name__ == "__main__":
+    main()
