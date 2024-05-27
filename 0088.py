7a3a046fb4a31a13b063345f0535fe156cf85202
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-05-27 10:31:46 -0300

    Update 0088.py

--- 0088.py
+++ 0088.py
@@ -1,22 +1,16 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        nums = nums1 + nums2
-        res = []
-        for num in nums:
-            if num != 0:
-                res.append(num)
-        return sorted(res)
-
+        nums1[m:] = nums2
+        return sorted(nums1)
 
 
 def main() -> None:
-    solution = Solution()
-    nums1 = [0]
-    m = 0
-    nums2 = [1]
-    n = 1
-    print(solution.merge(nums1, m, nums2, n))
-
+    sol = Solution()
+    nums1 = [1,2,3,0,0,0]
+    m = 3
+    nums2 = [2,5,6]
+    n = 3
+    print(sol.merge(nums1, m, nums2, n))
 
 if __name__ == "__main__":
     main()
