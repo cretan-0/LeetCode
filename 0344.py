3a084bc8e56103c47157ed3d71ea769ba2f39e47
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-06-09 09:22:02 -0300

    Update 0344.py

--- 0344.py
+++ 0344.py
@@ -13,9 +13,28 @@ def main() -> None:
 if __name__ == "__main__":
     main()
 
+# Time complexity O(n)
+# Space complexity O(1)
+
 # smart version
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         n = len(s)
         for i in range(n//2): # parcurgem stringul pana la jumatate
             s[i], s[n-1-i] = s[n-1-i], s[i] # swap intre primul element si ultimul element iterativ ;)
+
+# Time complexity O(n)
+# Space complexity O(1)
+
+# another smart version
+class Solution(object):
+    def reverseString(self, s):
+        left = 0
+        right = len(s)-1
+        while (left < right):
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left +=1
+            right -= 1
+        return s
