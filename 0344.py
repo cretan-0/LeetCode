3f06a85ff3e2d9e5755595498fc5eb831bbb9857
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-06-09 09:00:35 -0300

    Update 0344.py

--- 0344.py
+++ 0344.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         n = len(s)
-        for i in range(n//2):
-            s[i], s[n-1-i] = s[n-1-i], s[i]
+        for i in range(n//2): # parcurgem stringul pana la jumatate
+            s[i], s[n-1-i] = s[n-1-i], s[i] # swap intre primul element si ultimul element iterativ ;)
