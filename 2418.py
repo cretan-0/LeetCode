ecc1c851914a804b71d880288091b8c75c3417f8
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-08-05 18:42:30 -0300

    Create 2418.py

---
+++ 2418.py
@@ -0,0 +1,30 @@
+# Runtime: 77ms, Beats 79.77%
+# Memory: 12.15MB, 73.10%
+
+class Solution(object):
+    def sortPeople(self, names, heights):
+        hashtable = {}
+        for i, item in enumerate(heights):
+            if item in hashtable:
+                hashtable[item] += names[i]
+            else:
+                hashtable[item] = names[i]
+
+        hashtable = sorted(hashtable.items(), reverse=True)
+        
+        new_arr = []
+        
+        for i in hashtable:
+            new_arr.append(i[1])
+        return new_arr
+
+
+def main() -> None:
+    sol = Solution()
+    names = ["Mary","John","Emma"]
+    heights = [180,165,170]
+    print(sol.sortPeople(names, heights))
+
+if __name__ == "__main__":
+    main()
+
