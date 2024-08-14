9c32484957b3f95a9156899a853e3fff3dcf0ee9
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-08-14 11:25:10 -0300

    Update 0415.py

--- 0415.py
+++ 0415.py
@@ -2,3 +2,18 @@ class Solution(object):
     def addStrings(self, num1, num2):
         rez = int(num1) + int(num2)
         return str(rez)
+
+#v2
+class Solution(object):
+    def addStrings(self, num1, num2):
+        return str(int(num1)+int(num2))
+
+
+def main() -> None:
+    sol = Solution()
+    num1 = "11"
+    num2 = "123"
+    print(sol.addStrings(num1, num2))
+
+if __name__ == "__main__":
+    main()
