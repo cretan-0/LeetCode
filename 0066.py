b9e63cfb3b784e8b82b395567648eac911d4a232
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-09-24 02:25:25 -0300

    Update 0066.py

--- 0066.py
+++ 0066.py
@@ -8,3 +8,29 @@ class Solution(object):
         return digits
            
 # 93/111 testcases passed - > to be solved
+
+
+# 24/09/24
+
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        int_digits = int(''.join(map(str, digits)))
+        int_digits += 1
+        #int_digits = int_digits.split()
+        int_digits = str(int_digits)
+        res = [number for number in int_digits]
+        res = list(map(int, res))
+        return res
+
+
+
+def main() ->  None:
+    sol = Solution()
+    digits = [1,2,3]
+    print(sol.plusOne(digits))
+
+
+if __name__ == "__main__":
+    main()
