79073494c7b3d895418cbe5b0c115787a50babbc
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2024-09-25 03:53:19 -0300

    # Wrong Answer # 479 / 718 testcases passed

---
+++ 3043.py
@@ -0,0 +1,41 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        max = 0
+        for elem1 in arr1:
+            for elem2 in arr2:
+
+                cp_arr1 = [int(digit) for digit in str(elem1)]
+                cp_arr2 = [int(digit) for digit in str(elem2)]
+
+                #print("outer loops", cp_arr1, cp_arr2)
+
+                counter: int = 0
+                for i in range(min(len(cp_arr1), len(cp_arr2))):
+
+                    if cp_arr1[i] == cp_arr2[i]:
+                        counter += 1
+                    if cp_arr1[0] != cp_arr2[0]:
+                        counter = 0
+
+
+
+
+                    if counter > max:
+                        max = counter
+                   # counter = 0
+
+        return max
+
+
+# Wrong Answer
+# 479 / 718 testcases passed
+def main() -> None:
+    sol = Solution()
+    arr1 = [1124,9304,8119]
+    arr2 = [7782,3372,9864]
+    print(sol.longestCommonPrefix(arr1, arr2))
+
+if __name__ == '__main__':
+    main()
