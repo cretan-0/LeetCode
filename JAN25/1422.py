e78dd6d3074b58c40c4d91d97547a7a6959c684d
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2025-01-03 13:26:25 -0100

    Create 1422.py

---
+++ JAN25/1422.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxScore(self, s):
+        counter_max = 0
+        for i in range(len(s)-1):
+            left = s[:i+1]
+            right = s[i+1:]
+            counter_max = max(counter_max, left.count('0') + right.count('1'))
+        return counter_max
+
+
+def main() -> None:
+    sol = Solution()
+    s = "1111"
+    print(sol.maxScore(s))
+
+
+if __name__ == "__main__":
+    main()
