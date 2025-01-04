1dbaf0bdfe14b1f8beb96aab9a90bec5b1a2a5f1
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2025-01-04 13:42:46 -0100

    Create 2270.py

---
+++ JAN25/2270.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def waysToSplitArray(self, nums):
+        counter = 0
+        left_sum = 0
+        total_sum = sum(nums)
+
+        for i in range(len(nums)-1):
+            left_sum += nums[i]
+            right_sum = total_sum - left_sum
+            if left_sum >= right_sum:
+                counter +=1
+        return counter
+
+def main() -> None:
+    sol = Solution()
+    nums = [9,9,9]
+    print(sol.waysToSplitArray(nums))
+
+
+if __name__ == "__main__":
+    main()
