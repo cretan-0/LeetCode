591761c7c4220fc1c3c325e812f3df3b25d80a57
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2025-01-20 15:20:24 +0200

    Create 0349.py

---
+++ 0349.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        res = []
+        for i, j in zip(nums1, nums2):
+            if i in nums1 and i in nums2 and i not in res:
+                res.append(i)
+            if j in nums1 and j in nums2 and j not in res:
+                res.append(j)
+        return res
