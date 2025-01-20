9842027ddd0e345035117b0d0739b96adc7987d4
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2025-01-20 15:33:47 +0200

    Update 0349.py

--- 0349.py
+++ 0349.py
@@ -7,3 +7,13 @@ class Solution(object):
             if j in nums1 and j in nums2 and j not in res:
                 res.append(j)
         return res
+
+# V2
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        res = []
+        for i in nums1:
+            if i in nums2 and i not in res:
+                res.append(i)
+        return res
