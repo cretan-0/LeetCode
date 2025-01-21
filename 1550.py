6f1ea33c6397a54cf1f5781ba17a137a9f75c072
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2025-01-21 23:05:43 +0200

    Update 1550.py

--- 1550.py
+++ 1550.py
@@ -5,3 +5,27 @@ class Solution(object):
                 return True
         return False
         
+# v2
+
+v
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        count = 0
+        for elem in arr:
+            if elem % 2 == 1:
+                count += 1
+                if count == 3:
+                    return True
+            else:
+                count = 0
+        return False
+
+
+def main() -> None:
+    sol = Solution()
+    arr = [1,2,34,3,4,5,7,23,12]
+    print(sol.threeConsecutiveOdds(arr))
+
+
+if __name__ == "__main__":
+    main()
