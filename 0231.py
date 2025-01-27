f08a3541840d835f1f8fc31f4f9e4cdea0a9c369
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2025-01-27 12:12:06 +0200

    Update 0231.py

--- 0231.py
+++ 0231.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def isPowerOfTwo(self, n):
-        for i in range(0, 30):
+        for i in range(0, 31):
             if n == pow(2,i):
                 return True
         return False
