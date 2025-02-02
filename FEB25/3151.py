75d0cea2a0cda06b2a5a1864974f2830810489c8
qwerty00 <72599458+cretan-0@users.noreply.github.com>
2025-02-02 15:16:42 -0100

    Update 3151.py

--- FEB25/3151.py
+++ FEB25/3151.py
@@ -6,3 +6,19 @@ class Solution(object):
             if (nums[i] % 2 == 1 and nums[i+1] % 2 == 1) or (nums[i] % 2 == 0 and nums[i+1] % 2 == 0):
                 return False
         return True
+
+
+# v2
+
+'''
+Generally, odd + odd -> even 
+           even + even -> even
+           even + odd -> odd
+'''
+
+class Solution(object):
+    def isArraySpecial(self, nums):
+        for i in range(len(nums)-1):
+            if (nums[i]+nums[i+1]) % 2 == 0:
+                return False
+        return True
